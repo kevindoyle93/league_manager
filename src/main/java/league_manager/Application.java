bbff4312bda7c7f93d7bb79d34742209175b2a7c
Kevin Doyle <C13547287@mydit.ie>
2017-11-12 23:20:11 +0000

    Create utility class to mock application data

--- src/main/java/league_manager/Application.java
+++ src/main/java/league_manager/Application.java
@@ -1,10 +1,13 @@
 package league_manager;
 
+import league_manager.teams.Team;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    public static Team[] TEAMS = DataMocker.CREATE_MOCK_TEAMS();
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
